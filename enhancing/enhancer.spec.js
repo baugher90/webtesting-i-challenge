dbe765880c14b3fd9ea5368673021fde7d683aed
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-05-20 16:24:02 -0400

    generic tests written

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,6 +1,26 @@
-const enhancer = require('./enhancer.js');
+const {succeed, fail, repair, get} = require('./enhancer.js');
 // test away!
 //============================================ succeed Tests
+describe('enhancing.js', () => {
+    describe('succeed()', () => {
+        it.todo('', () => {})
+    })
+
 //============================================ fail Tests
+
+    describe('fail()', () => {
+        it.todo('', () => {})
+    })
+
 //============================================ repair Tests
+
+    describe('repair()', () => {
+        it.todo('', () => {})
+    })
+
 //============================================ get Tests
+
+    describe('get()', () => {
+        it.todo('', () => {})
+    })
+})
